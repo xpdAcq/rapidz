1f459864d1d4a1201b81ed01f301a44c9df56ecc
Christopher J. Wright <cjwright4242@gmail.com>
2019-03-01 19:31:30 -0500

    better graph plotting via sets

--- examples/plot_graph.py
+++ examples/plot_graph.py
@@ -10,4 +10,15 @@ n2 = n1.map(add)
 n3 = n2.zip(source3)
 L = n3.sink_to_list()
 
-n2.visualize()
+
+n2.visualize("simple.png")
+
+# Rapidz can also visualize more complex graphs
+n4 = source1.combine_latest(source2, n1, emit_on=(0, 1))
+n4.connect(n3)
+nm = n4.scatter(backend='thread').map(add)
+
+# Rapidz can even graph cyclical graphs!
+n3.connect(source3)
+
+n2.visualize("complex.png")
--- rapidz/tests/test_graph.py
+++ rapidz/tests/test_graph.py
@@ -5,10 +5,30 @@ import pytest
 
 nx = pytest.importorskip("networkx")
 
-from rapidz import Stream, create_graph, visualize
+from rapidz import (
+    Stream,
+    create_graph,
+    visualize,
+    build_node_set,
+)
 from rapidz.utils_test import tmpfile
 
 from ..graph import _clean_text
+
+
+def test_build_node_set():
+    source1 = Stream(stream_name="source1")
+    source2 = Stream(stream_name="source2")
+
+    n1 = source1.zip(source2)
+    n2 = n1.map(add)
+    n2.connect(source1)
+
+    s = set()
+    build_node_set(n2, s)
+    print(s)
+    for node in [source1, source2, n1, n2]:
+        assert node in s
 
 
 def test_create_graph():
@@ -17,7 +37,24 @@ def test_create_graph():
 
     n1 = source1.zip(source2)
     n2 = n1.map(add)
-    n2.sink(source1.emit)
+    sink = n2.sink(source1.emit)
+
+    g = nx.DiGraph()
+    create_graph(n2, g)
+    for t in [hash(a) for a in [source1, source2, n1, n2, sink]]:
+        assert t in g
+    for edge_nodes in [(source1, n1), (source2, n1), (n1, n2), (n2, sink)]:
+        edge = tuple([hash(n) for n in edge_nodes])
+        assert edge in g.edges
+
+
+def test_create_cyclic_graph():
+    source1 = Stream(stream_name="source1")
+    source2 = Stream(stream_name="source2")
+
+    n1 = source1.zip(source2)
+    n2 = n1.map(add)
+    n2.connect(source1)
 
     g = nx.DiGraph()
     create_graph(n2, g)
