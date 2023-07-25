c40eb268ccab9ffa1a1779d84fdf77c472dd4cbf
Simon Billinge <simon.billinge@gmail.com>
2023-07-25 09:44:28 +0200

    bumped version to 0.2.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ tests = [p + ".tests" for p in packages]
 
 setup(
     name="rapidz",
-    version='0.2.1',
+    version='0.2.2',
     description="Streams",
     url="http://github.com/xpdAcq/rapidz/",
     maintainer="Simon Billinge",
